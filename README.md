d2ef8e8bec777cc1f8a34e9933ed941d11c545af
Karsten Taylor <karstentaylor0@gmail.com>
2020-06-03 13:50:28 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ### Boilerplate app structure for Quiz App project
+
+Mike F & Karsten
--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+//The starting screen should have a button that users can click to start the quiz.
+
+//Users should be prompted through a series of at least 5 multiple choice questions that they can answer.
+
+//Users should be asked questions 1 after the other.
+
+//Users should only be prompted with 1 question at a time.
+
+//Users should not be able to skip questions.
+
+//Users should also be able to see which question they're on (for instance, "7 out of 10")
+//and their current score ("5 correct, 2 incorrect").
+
+//Upon submitting an answer, users should:
+//receive textual feedback about their answer. If they were incorrect, they should be told the correct answer.
+//be moved onto the next question (or interact with an element to move on).
+
+//Users should be shown their overall score at the end of the quiz. In other words, how many questions
+//they got right out of the total questions asked.
+
+//Users should be able to start a new quiz
+
 /**
  * Example store structure
  */
@@ -6,35 +28,25 @@ const store = {
   questions: [
     {
       question: 'What color is broccoli?',
-      answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
-      ],
-      correctAnswer: 'green'
+      answers: ['red', 'orange', 'pink', 'green'],
+      correctAnswer: 'green',
     },
     {
       question: 'What is the current year?',
-      answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
-      ],
-      correctAnswer: '2019'
-    }
+      answers: ['1970', '2015', '2019', '2005'],
+      correctAnswer: '2019',
+    },
   ],
   quizStarted: false,
   questionNumber: 0,
-  score: 0
+  score: 0,
 };
 
 /**
- * 
+ *
  * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
+ *
+ * Your app should include a render() function, that regenerates the view each time the store is updated.
  * See your course material, consult your instructor, and reference the slides for more details.
  *
  * NO additional HTML elements should be added to the index.html file.
@@ -42,7 +54,7 @@ const store = {
  * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
  *
  * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- * 
+ *
  */
 
 /********** TEMPLATE GENERATION FUNCTIONS **********/
